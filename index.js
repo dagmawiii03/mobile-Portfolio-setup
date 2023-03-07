4b5e1c62653df79a168389cba2e123e7af4e841c
dagmawiii03 <dagmawi.yoha03@gmail.com>
2023-03-07 16:52:14 +0300

    change to single quote in js file

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Toogle Mobile Menu javascript
 
-const menu = document.querySelector(".mobileMenu");
+const menu = document.querySelector('.mobileMenu');
 
-const mobileMenu = document.querySelector(".menu-mobile");
+const mobileMenu = document.querySelector('.menu-mobile');
 
 const toggleMenu = () => {
-  mobileMenu.classList.toggle("showMenu");
+  mobileMenu.classList.toggle('showMenu');
 };
 
-menu.addEventListener("click", () => toggleMenu());
+menu.addEventListener('click', () => toggleMenu());
 
-mobileMenu.addEventListener("click", () => toggleMenu());
+mobileMenu.addEventListener('click', () => toggleMenu());
